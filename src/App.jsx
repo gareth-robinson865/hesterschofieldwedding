82c305bb78b766cd663fc0489780650853a4a093
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-01-29 13:48:15 +0000

    form created with text input

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,13 @@ function App() {
       
       <p>Welcome message</p>
       <p>Name input</p> 
+      <form>
+        <label>Who is responding</label> <br></br>
+        <input type="text" id="name" name="name" placeholder="who is responding" required></input>
+      </form>
+      <form>
+        <input type="radio"></input>
+      </form>
       <p>Are you attending</p>
       <br></br>
       <h3>options if atteding is true</h3>
