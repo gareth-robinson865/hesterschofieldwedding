6ef7e71f5907a630ade6eab3786c3cd035b747c3
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-02-05 14:55:01 +0000

    updated the error causing the code to be called on every refresh

--- src/App.jsx
+++ src/App.jsx
@@ -42,10 +42,10 @@ function App() {
         <input type="text" id="name" name="name"  required></input><br></br>
 
         <p>Will you be attending?</p>
-        <input type="radio" id="yes" name="attending" value="YES" onChange={updateAttending()}></input>
+        <input type="radio" id="yes" name="attending" value="YES" onChange={updateAttending}></input>
         <label htmlFor="yes">Yes, i will be attending</label>
 
-        <input type="radio" id="no" name="attending" value="NO" onChange={updateNotAttending()}></input>
+        <input type="radio" id="no" name="attending" value="NO" onChange={updateNotAttending}></input>
         <label htmlFor="no">No, i will not be attending</label>
       </form> 
       
