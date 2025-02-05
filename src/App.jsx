c7d576812826f61b6212d191aa70ef22c11ecf31
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-02-05 14:53:47 +0000

    functions updated to change the state

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,16 @@ function App() {
   const [showAttending, setShowAttending] = useState(false)
   const [showNotAttending, setShowNotAttending] = useState(false)
 
+  function updateAttending() {
+    setShowAttending(true);
+    setShowNotAttending(false);
+  }
+
+  function updateNotAttending() {
+    setShowAttending(false);
+    setShowNotAttending(true)
+  }
+
   console.log(showAttending)
   console.log(showNotAttending)
 
@@ -32,10 +42,10 @@ function App() {
         <input type="text" id="name" name="name"  required></input><br></br>
 
         <p>Will you be attending?</p>
-        <input type="radio" id="yes" name="attending" value="YES" onChange={() => setShowAttending(true)}></input>
+        <input type="radio" id="yes" name="attending" value="YES" onChange={updateAttending()}></input>
         <label htmlFor="yes">Yes, i will be attending</label>
 
-        <input type="radio" id="no" name="attending" value="NO" onChange={() => setShowNotAttending(true)}></input>
+        <input type="radio" id="no" name="attending" value="NO" onChange={updateNotAttending()}></input>
         <label htmlFor="no">No, i will not be attending</label>
       </form> 
       
