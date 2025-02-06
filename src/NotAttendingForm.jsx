c789930f9fb8a8c4340e66af4bdfff45db0bcc4d
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-02-06 17:02:45 +0000

    placeholder created

--- src/NotAttendingForm.jsx
+++ src/NotAttendingForm.jsx
@@ -2,7 +2,7 @@ export default function NotAttendingForm() {
     return(
         <form id="notAttending">
             <label htmlFor="message">Sorry you cannot make it, feel free to send us a message</label><br></br>
-            <textarea id="message" rows="4" cols="50"></textarea>
+            <textarea id="message" rows="4" cols="50" placeholder="type message here..."></textarea>
             <br></br>
             <br></br>
             <button type="submit">Click to send</button>
