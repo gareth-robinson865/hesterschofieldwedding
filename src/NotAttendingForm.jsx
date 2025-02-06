a532e3dbfd9cd66944f1c0e71e94a1a7bd5c8f8a
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-02-06 17:13:32 +0000

    line break added for style

--- src/NotAttendingForm.jsx
+++ src/NotAttendingForm.jsx
@@ -1,6 +1,7 @@
 export default function NotAttendingForm() {
     return(
         <form id="notAttending">
+            <br></br>
             <label htmlFor="message">Sorry you cannot make it, feel free to send us a message</label><br></br>
             <br></br>
             <textarea id="message" rows="4" cols="50" placeholder="type message here..."></textarea>
