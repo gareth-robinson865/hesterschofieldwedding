65f6b1addf3bed661ea6c6eb915ebf7679b7c342
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-02-06 17:33:40 +0000

    welcome form code removed from main app. code is now in component form and working

--- src/App.jsx
+++ src/App.jsx
@@ -25,18 +25,7 @@ function App() {
       <Title />
       <WelcomeMessage />
       <WelcomeForm updateAttending={updateAttending} updateNotAttending={updateNotAttending} />
-      <form id="generic">
-        <label htmlFor="name">Who is responding</label> <br></br>
-        <input type="text" id="name" name="name"  required></input><br></br>
-
-        <p>Will you be attending?</p>
-        <input type="radio" id="yes" name="attending" value="YES" onChange={updateAttending}></input>
-        <label htmlFor="yes">Yes, i will be attending</label>
-
-        <input type="radio" id="no" name="attending" value="NO" onChange={updateNotAttending}></input>
-        <label htmlFor="no">No, i will not be attending</label>
-      </form>
-
+      
       { showNotAttending && <NotAttendingForm />}
 
       { showAttending && <AttendingForm /> }
