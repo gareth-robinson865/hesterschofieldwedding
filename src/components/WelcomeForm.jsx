f2b9bcd0bf07938293398492836a90c3c1099d7e
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-02-06 16:50:34 +0000

    info put into welcome form but not working as cannot pass state from child to parent. May not use this method

--- src/components/WelcomeForm.jsx
+++ src/components/WelcomeForm.jsx
@@ -1,5 +1,33 @@
+import { useState } from 'react';
+
 export default function WelcomeForm() {
+    const [showAttending, setShowAttending] = useState(false)
+    const [showNotAttending, setShowNotAttending] = useState(false)
+    
+    function updateAttending() {
+        setShowAttending(true);
+        setShowNotAttending(false);
+    }
+    
+    function updateNotAttending() {
+        setShowAttending(false);
+        setShowNotAttending(true)
+    }
+    
+    console.log(showAttending)
+    console.log(showNotAttending)
+    
     return (
+        <form id="generic">
+            <label htmlFor="name">Who is responding</label> <br></br>
+            <input type="text" id="name" name="name"  required></input><br></br>
         
+            <p>Will you be attending?</p>
+            <input type="radio" id="yes" name="attending" value="YES" onChange={updateAttending}></input>
+            <label htmlFor="yes">Yes, i will be attending</label>
+        
+            <input type="radio" id="no" name="attending" value="NO" onChange={updateNotAttending}></input>
+            <label htmlFor="no">No, i will not be attending</label>
+        </form> 
     )
 }
