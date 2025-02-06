55182a163d8751e86ff540071a424be5891a92b7
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-02-06 16:15:38 +0000

    not attending form component created and content removed from app

---
+++ src/NotAttendingForm.jsx
@@ -0,0 +1,10 @@
+export default function NotAttendingForm() {
+    return(
+        <form id="notAttending">
+            <label htmlFor="message">Sorry you cannot make it, feel free to send us a message</label><br></br>
+            <input type="textarea" id="message"></input>
+            <br></br>
+            <button type="submit">Click to send</button>
+        </form>
+    )
+}
