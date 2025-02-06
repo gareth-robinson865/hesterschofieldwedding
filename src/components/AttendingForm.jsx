e4330d508161f08fe5da5c3db8b998837587c1c4
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-02-06 16:22:48 +0000

    variables imported into the attendingform component

--- src/components/AttendingForm.jsx
+++ src/components/AttendingForm.jsx
@@ -1,5 +1,16 @@
 export default function AttendingForm() {
+    const starter1 = 'Baked Goats Cheese Crottin';
+      const starter2 = 'Thai Spiced Fish Cakes';
+      const starter3 = 'Warm Asparagus and Tomato Tart (Vegan)';
+    
+      const main1 = 'Fillet of Yorkshire Beef en Croute';
+      const main2 = 'Fillet of Salmon en Croute';
+      const main3 = 'Butternut Squash and Lentil en Croute (Vegan)';
+    
+      const dessert1 = 'Sticky Toffee Pudding';
+      const dessert2 = 'Chocolate Brownie';
+      const dessert3 = 'Bowl of Fruit (Vegan)';
     return(
-        
+
     )
 }
