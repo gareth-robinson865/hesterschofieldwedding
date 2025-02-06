c98fc137e55093ffa25d52d9f1d97086d8fd89b5
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-02-06 16:56:40 +0000

    double line break added for style

--- src/NotAttendingForm.jsx
+++ src/NotAttendingForm.jsx
@@ -4,6 +4,7 @@ export default function NotAttendingForm() {
             <label htmlFor="message">Sorry you cannot make it, feel free to send us a message</label><br></br>
             <input type="textarea" id="message"></input>
             <br></br>
+            <br></br>
             <button type="submit">Click to send</button>
         </form>
     )
