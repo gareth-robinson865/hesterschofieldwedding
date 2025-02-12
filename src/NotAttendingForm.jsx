7737a9fa12ce3a2b0bb2a9733ec84aaa6683d2b5
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-02-12 13:35:03 +0000

    name form added to the not attending form

--- src/NotAttendingForm.jsx
+++ src/NotAttendingForm.jsx
@@ -4,6 +4,8 @@ export default function NotAttendingForm() {
             <br></br>
             <label htmlFor="message">Sorry you cannot make it, feel free to send us a message</label><br></br>
             <br></br>
+            <label htmlFor="name">Who is responding</label> <br></br>
+            <input type="text" id="name" name="name"  required></input><br></br>
             <textarea id="message" rows="4" cols="50" placeholder="type message here..."></textarea>
             <br></br>
             <br></br>
