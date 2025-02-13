93025a981e317fef56b2a930c5251bedf73db72f
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-02-13 15:14:54 +0000

    semi colon added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import './App.css'
+import './App.css';
 import Title from './components/Title';
 import WelcomeMessage from './components/welcomeMessage';
 import WelcomeForm from './components/WelcomeForm';
