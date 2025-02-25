3b7ade30332acd90b21a7649d2f496ce5918cbf8
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-02-25 16:21:16 +0000

    Title changed

--- src/components/title.jsx
+++ src/components/title.jsx
@@ -2,6 +2,6 @@ import './Title.css'
 
 export default function Title() {
     return (
-        <h1 id="title">Join us on our Special Day</h1>
+        <h1 id="title">The Hester Schofield Wedding</h1>
     )
 }
