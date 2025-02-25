df38dd96cbee044be53b62115e1c85b2cf4dc9b4
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-02-25 16:21:27 +0000

    Welcome message changed

--- src/components/welcomeMessage.jsx
+++ src/components/welcomeMessage.jsx
@@ -2,6 +2,6 @@ import './WelcomeMessage.css'
 
 export default function WelcomeMessage() {
     return (
-        <h4 id="welcomeMessage">Hester-Schofield Wedding</h4>
+        <h4 id="welcomeMessage">Friday 15th August 2025</h4>
     )
 }
