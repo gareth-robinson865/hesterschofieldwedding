3e8aa2c3f45f8f53c811000b9270e90d681f2198
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-02-27 20:36:31 +0000

    main file created and meals put on

---
+++ src/components/Mains.jsx
@@ -0,0 +1,9 @@
+export default function Mains() {
+    const main1 = 'Fillet of Yorkshire Beef en Croute';
+    const main2 = 'Fillet of Salmon en Croute';
+    return (
+    <div>
+
+    </div>
+  )
+}
