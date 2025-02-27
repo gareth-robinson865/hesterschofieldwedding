6f7bd4dcb6ea1da745f9f2157e250dd3ceca896a
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-02-27 20:43:48 +0000

    label and selector moved into component

--- src/components/Mains.jsx
+++ src/components/Mains.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export default function Mains() {
     const main1 = 'Fillet of Yorkshire Beef en Croute';
     const main2 = 'Fillet of Salmon en Croute';
@@ -7,6 +9,23 @@ export default function Mains() {
     return (
     <div>
         <h4>Mains</h4>
+        <label htmlFor="main1">{main1}</label>
+        <input 
+            type="radio" 
+            id="main1" 
+            name="mains" 
+            value="beef" 
+            onChange={(e) => setMain(e.target.value)}>
+        </input> <br></br>
+
+        <label htmlFor="main2">{main2}</label>
+        <input 
+            type="radio" 
+            id="main2" 
+            name="mains" 
+            value="salmon" 
+            onChange={(e) => setMain(e.target.value)}>
+        </input> <br></br>
     </div>
   )
 }
