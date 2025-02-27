7bce2688e1f782c22040c8ae7a22205e1f7732a9
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-02-27 20:38:19 +0000

    title pasted

--- src/components/Mains.jsx
+++ src/components/Mains.jsx
@@ -1,9 +1,12 @@
 export default function Mains() {
     const main1 = 'Fillet of Yorkshire Beef en Croute';
     const main2 = 'Fillet of Salmon en Croute';
+
+    const [main, setMain] = useState('');
+
     return (
     <div>
-
+        <h4>Mains</h4>
     </div>
   )
 }
