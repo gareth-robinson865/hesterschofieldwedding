6fa4ff9beb1b4937d089d6364f126d33abab52f4
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-02-28 11:35:55 +0000

    functions imported into component

--- src/components/AdditionalInfo.jsx
+++ src/components/AdditionalInfo.jsx
@@ -1,7 +1,16 @@
-export default function AdditionalInfo() {
+/* eslint-disable react/prop-types */
+export default function AdditionalInfo({ setMoney, setDrink, setSong}) {
   return (
     <div>
-        AdditionalInfo
+        <label htmlFor="moneySent">Have you sent the money over for the bar?</label>
+        <input type="checkbox" id="moneySent" name="moneySent" onChange={(e) => setMoney(e.target.value)}></input>
+        <br></br>
+        <label htmlFor="favSong">Let us know your favourite song</label>
+        <input type="text" id="favSong" name="favSong" onChange={(e) => setSong(e.target.value)}></input>
+        <br></br>
+        <label htmlFor="favDrink">Let us know your favourite drink</label>
+        <input type="text" id="favDrink" name="favDrink" onChange={(e) => setDrink(e.target.value)}></input>
+        <br></br>
     </div>
   )
 }
