e0d3e33ff586b7c0642f4694460fe1b120a9dc63
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-02-28 10:17:04 +0000

    info brought from attending form

--- src/components/DietaryInfo.jsx
+++ src/components/DietaryInfo.jsx
@@ -1,7 +1,12 @@
 export default function DietaryInfo() {
   return (
     <div>
-        DietaryInfo
+        <h4>Your info</h4>
+        <label htmlFor="name">Who is responding</label>
+        <input type="text" id="name" name="name"  required onChange={(e) => setName(e.target.value)}></input><br></br>
+            <br></br>
+        <label htmlFor="dietary">Let us know if you have any dietary requirements</label>
+        <input type="text" id="dietary" name="dietary" onChange={(e) => setDietary(e.target.value)}></input>
     </div>
   )
 }
