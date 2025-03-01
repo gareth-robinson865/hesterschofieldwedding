00bc2f976728a60101a05cc903b8553b266ecc6c
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-03-01 15:53:37 +0000

    additional radio buttons to try handle state and conditional rendering

--- src/components/DietaryInfo.jsx
+++ src/components/DietaryInfo.jsx
@@ -13,12 +13,29 @@ export default function DietaryInfo({ setDietary, setName, setIsVegan}) {
         </input>
         <br></br>
         <br></br>
-        <label htmlFor="vegan">Are you vegan, vegetarian or have dietary requirements</label>
+        <h4>Are you vegan, vegetarian or have dietary requirements</h4>
+        <label htmlFor="vegan">Vegan</label>
         <input 
             type="radio" 
             id="vegan"
             name="vegan" 
             value="vegan" 
+            onChange={(e) => setIsVegan(e.target.value)}>
+        </input>
+        <label htmlFor="vegetarian">Vegetarian</label>
+        <input 
+            type="radio" 
+            id="vegetarian"
+            name="vegetarian" 
+            value="vegetarian" 
+            onChange={(e) => setIsVegan(e.target.value)}>
+        </input>
+        <label htmlFor="none">None</label>
+        <input 
+            type="radio" 
+            id="normal"
+            name="normal" 
+            value="normal" 
             onChange={(e) => setIsVegan(e.target.value)}>
         </input>
         <label htmlFor="dietary">Let us know if you have any dietary requirements</label>
