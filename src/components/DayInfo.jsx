5f709291dcd4c25bd8b341e1f7f04530ccddcaad
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-03-03 21:32:03 +0000

    day info created

--- src/components/DayInfo.jsx
+++ src/components/DayInfo.jsx
@@ -1,7 +1,17 @@
 export default function DayInfo() {
   return (
     <div>
-        
+        <h2>Info for the day</h2>
+        <p>12:00AM to 12:15PM : Guests arrive</p>
+        <p>12:30 to 1:15PM : Ceremony</p>
+        <p>1:30PM to 3:00PM : Drinks reception and canapes</p>
+        <p>3:00PM to 3:30PM : Speeches</p>
+        <p>3:30PM to 5:30PM : Wedding Breakfast</p>
+        <p>6:00PM to 7:00PM : Cocktail o`clock</p>
+        <p>7:00PM : Evening Guest Arrival</p>
+        <p>7:30PM : First Dance</p>
+        <p>7:45PM to 11:45PM : Party Time</p>
+        <p>12:00PM : Carriages</p>
     </div>
   )
 }
