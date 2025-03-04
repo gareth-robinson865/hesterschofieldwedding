0d12c5275cb2f1c8872781dccf5bdf170aaaf619
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-03-04 15:53:00 +0000

    line breaks added for styling

--- src/components/AdditionalInfo.jsx
+++ src/components/AdditionalInfo.jsx
@@ -5,11 +5,14 @@ export default function AdditionalInfo({ setMoney, setDrink, setSong}) {
         <label htmlFor="moneySent">Have you sent the money over for the bar?</label>
         <input type="checkbox" id="moneySent" name="moneySent" onChange={(e) => setMoney(e.target.value)}></input>
         <br></br>
+        <br></br>
         <label htmlFor="favSong">Let us know your favourite song</label>
         <input type="text" id="favSong" name="favSong" onChange={(e) => setSong(e.target.value)}></input>
         <br></br>
+        <br></br>
         <label htmlFor="favDrink">Let us know your favourite drink</label>
         <input type="text" id="favDrink" name="favDrink" onChange={(e) => setDrink(e.target.value)}></input>
+        <br></br>
         <br></br>
     </div>
   )
