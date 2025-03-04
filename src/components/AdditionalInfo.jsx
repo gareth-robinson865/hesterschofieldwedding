5841db776a7e5d38f2615ea37f0ac0b0195aa9b2
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-03-04 16:07:08 +0000

    line break added

--- src/components/AdditionalInfo.jsx
+++ src/components/AdditionalInfo.jsx
@@ -9,6 +9,7 @@ export default function AdditionalInfo({ setMoney, setDrink, setSong, setName })
             name="name"  
             required onChange={(e) => setName(e.target.value)}>
         </input>
+        <br></br>
         <label htmlFor="moneySent">Have you sent the money over for the bar?</label>
         <input type="checkbox" id="moneySent" name="moneySent" onChange={(e) => setMoney(e.target.value)}></input>
         <br></br>
