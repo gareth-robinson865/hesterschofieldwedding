56a4acb4ef506dbfee2a0e3c324d84ff4e5c1b01
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-03-04 15:03:30 +0000

    div put around the food infor to facilitate conditional rendering

--- src/components/DietaryInfo.jsx
+++ src/components/DietaryInfo.jsx
@@ -14,41 +14,44 @@ export default function DietaryInfo({ setDietary, setName, setIsVegan, setNonVeg
             name="name"  
             required onChange={(e) => setName(e.target.value)}>
         </input>
-        <h4>Are you vegan, vegetarian or have dietary requirements</h4>
-        <label htmlFor="vegan">Vegan</label>
-        <input 
-            type="radio" 
-            id="vegan"
-            name="diet" 
-            value="vegan" 
-            onChange={(e) => setIsVegan(e.target.value)}>
-        </input>
-        <label htmlFor="vegetarian">Vegetarian</label>
-        <input 
-            type="radio" 
-            id="vegetarian"
-            name="diet" 
-            value="vegetarian" 
-            onChange={(e) => setIsVegan(e.target.value)}>
-        </input>
-        <label htmlFor="none">None</label>
-        <input 
-            type="radio" 
-            id="normal"
-            name="diet" 
-            value="normal" 
-            onChange={(e) => setNonVeg(e.target.value)}>
-        </input>
-        <br></br>
-        <br></br>
-        <label htmlFor="dietary">Let us know if you have any dietary requirements</label>
-        <br></br>
-        <input 
-            type="text" 
-            id="dietary" 
-            name="dietary" 
-            onChange={(e) => setDietary(e.target.value)}>
-        </input>
+        <h4></h4>
+        <div id="foodStuff">
+            <h4>Are you vegan, vegetarian or have dietary requirements</h4>
+            <label htmlFor="vegan">Vegan</label>
+            <input 
+                type="radio" 
+                id="vegan"
+                name="diet" 
+                value="vegan" 
+                onChange={(e) => setIsVegan(e.target.value)}>
+            </input>
+            <label htmlFor="vegetarian">Vegetarian</label>
+            <input 
+                type="radio" 
+                id="vegetarian"
+                name="diet" 
+                value="vegetarian" 
+                onChange={(e) => setIsVegan(e.target.value)}>
+            </input>
+            <label htmlFor="none">None</label>
+            <input 
+                type="radio" 
+                id="normal"
+                name="diet" 
+                value="normal" 
+                onChange={(e) => setNonVeg(e.target.value)}>
+            </input>
+            <br></br>
+            <br></br>
+            <label htmlFor="dietary">Let us know if you have any dietary requirements</label>
+            <br></br>
+            <input 
+                type="text" 
+                id="dietary" 
+                name="dietary" 
+                onChange={(e) => setDietary(e.target.value)}>
+            </input>
+        </div>
     </div>
   )
 }
