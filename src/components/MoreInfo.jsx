9d1b67955d5f346861bad01fbcae71405e3aec43
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-03-05 20:50:55 +0000

    info pasted into the new component

--- src/components/MoreInfo.jsx
+++ src/components/MoreInfo.jsx
@@ -1,7 +1,22 @@
 export default function MoreInfo() {
   return (
     <div>
-        
+        <div id="info">
+        <h5>Church</h5>
+        <p>St Mary`s Church has plenty of roadside parking. Please refrain from taking pictures during the ceremony, our photographer has this covered.</p>
+        <h5>Reception</h5>
+        <p>Approx 5 minutes drive to the reception venue of Castle Farm Barn, Knaresborough, please see your invite for directions.</p>
+        <p>Please look out for the wedding signs on arrival and follow them down the long bumpy road. </p>
+        <p>Cars can be left on the large car park field and collected anytime before MIDDAY SUNDAY 17th AUGUST.</p>
+        <p>The site will be open at any time for you to access your vehicle before then.</p>
+        <h5>Gifts</h5>
+        <p>Please do not feel obliged to buy us a gift, your company is more than enough. If you would like to give something we would appreciate a contribution to our honeymoon fund.</p>
+        <h5>Outfits</h5>
+        <p>We would love it if you could join us and wear black tie and cocktail party style wedding attire.</p>
+        <p>There is a lot of uneven groundat the venue, so stilettos are not advised.</p>
+        <h5>Travel / accomodation</h5>
+        <p></p>
+      </div>
     </div>
   )
 }
