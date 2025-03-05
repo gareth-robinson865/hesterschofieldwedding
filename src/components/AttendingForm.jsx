0853f42fcc95d7d375b32087cb9047b62f04e8b6
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-03-05 21:06:59 +0000

    prevent default on the button click created

--- src/components/AttendingForm.jsx
+++ src/components/AttendingForm.jsx
@@ -41,6 +41,7 @@ export default function AttendingForm() {
       }
 
       const handleMoreInfo = (e) => {
+        e.preventDefault()
         setShowInfo(false)
         setShowMoreInfo(true)
       }
