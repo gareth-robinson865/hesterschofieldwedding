e13ce9d2e232dcc8a2ecc65f6c2004cf4ecdba43
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-03-05 20:57:36 +0000

    required added to fav song

--- src/components/AdditionalInfo.jsx
+++ src/components/AdditionalInfo.jsx
@@ -12,11 +12,11 @@ export default function AdditionalInfo({ setMoney, setDrink, setSong, setName })
         <br></br>
         <br></br>
         <label htmlFor="moneySent">Have you sent the money over for the bar?</label>
-        <input type="checkbox" id="moneySent" name="moneySent" onChange={(e) => setMoney(e.target.value)}></input>
+        <input type="checkbox" id="moneySent" name="moneySent"  onChange={(e) => setMoney(e.target.value)}></input>
         <br></br>
         <br></br>
         <label htmlFor="favSong">Let us know your favourite song</label>
-        <input type="text" id="favSong" name="favSong" onChange={(e) => setSong(e.target.value)}></input>
+        <input type="text" id="favSong" name="favSong"  required onChange={(e) => setSong(e.target.value)}></input>
         <br></br>
         <br></br>
         <label htmlFor="favDrink">Let us know your favourite drink</label>
