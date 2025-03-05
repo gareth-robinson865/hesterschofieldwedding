fb3ab80c190f20c32d8d16f8bae7052f00896b34
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-03-05 20:44:06 +0000

    divs given id for styling

--- src/components/DayInfo.jsx
+++ src/components/DayInfo.jsx
@@ -1,7 +1,7 @@
 export default function DayInfo() {
   return (
-    <div>
-      <div>
+    <div id="mainInfo">
+      <div id="schedule">
         <h2>Info for the day</h2>
         <p>12:00AM to 12:15PM : Guests arrive</p>
         <p>12:30 to 1:15PM : Ceremony</p>
@@ -14,7 +14,7 @@ export default function DayInfo() {
         <p>7:45PM to 11:45PM : Party Time</p>
         <p>12:00PM : Carriages</p>
       </div>
-      <div>
+      <div id="info">
         <h5>Church</h5>
         <p>St Mary`s Church has plenty of roadside parking. Please refrain from taking pictures during the ceremony, our photographer has this covered.</p>
         <h5>Reception</h5>
