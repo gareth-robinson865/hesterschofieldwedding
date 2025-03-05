d3f26d9e0ca219ed57f4b0188846a5083cff28e4
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-03-05 21:35:04 +0000

    travel info created

--- src/components/MoreInfo.jsx
+++ src/components/MoreInfo.jsx
@@ -15,7 +15,13 @@ export default function MoreInfo() {
         <p>We would love it if you could join us and wear black tie and cocktail party style wedding attire.</p>
         <p>There is a lot of uneven groundat the venue, so stilettos are not advised.</p>
         <h5>Travel / accomodation</h5>
-        <p></p>
+        <p>If you are joining us from further afield, there are many places close by that you can
+        stay. The reception venue is 5 minutes’ drive from Knaresborough centre where there are many places to stay, or Harrogate which is around a 20-minute drive away.</p>
+        <h5>Local taxi companies</h5>
+        <p>Blue Line: 01423 530830 (Harrogate Based)</p>
+        <p>SJK: 0113 250 5222 (Yeadon Based)</p>
+        <p>Knaresborough cars: 01423 552266 (small company based in Knaresborough)</p>
+        <p>Please note, as the venue is difficult to find with the postcode and Uber is temperamental in Knaresborough and Harrogate area, please ensure that all taxis are booked in advance.</p>
       </div>
     </div>
   )
