8307b7107919c26eed9b0e97d4b0a122ed9f1af9
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-03-05 21:38:15 +0000

    some info removed into new component and div removed

--- src/components/MoreInfo.jsx
+++ src/components/MoreInfo.jsx
@@ -1,7 +1,7 @@
 export default function MoreInfo() {
   return (
     <div>
-        <div id="info">
+        
         <h5>Church</h5>
         <p>St Mary`s Church has plenty of roadside parking. Please refrain from taking pictures during the ceremony, our photographer has this covered.</p>
         <h5>Reception</h5>
@@ -11,18 +11,7 @@ export default function MoreInfo() {
         <p>The site will be open at any time for you to access your vehicle before then.</p>
         <h5>Gifts</h5>
         <p>Please do not feel obliged to buy us a gift, your company is more than enough. If you would like to give something we would appreciate a contribution to our honeymoon fund.</p>
-        <h5>Outfits</h5>
-        <p>We would love it if you could join us and wear black tie and cocktail party style wedding attire.</p>
-        <p>There is a lot of uneven groundat the venue, so stilettos are not advised.</p>
-        <h5>Travel / accomodation</h5>
-        <p>If you are joining us from further afield, there are many places close by that you can
-        stay. The reception venue is 5 minutes’ drive from Knaresborough centre where there are many places to stay, or Harrogate which is around a 20-minute drive away.</p>
-        <h5>Local taxi companies</h5>
-        <p>Blue Line: 01423 530830 (Harrogate Based)</p>
-        <p>SJK: 0113 250 5222 (Yeadon Based)</p>
-        <p>Knaresborough cars: 01423 552266 (small company based in Knaresborough)</p>
-        <p>Please note, as the venue is difficult to find with the postcode and Uber is temperamental in Knaresborough and Harrogate area, please ensure that all taxis are booked in advance.</p>
-      </div>
+      
     </div>
   )
 }
