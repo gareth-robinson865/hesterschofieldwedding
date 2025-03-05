05f60310ddb9e1b75a7a8875abfff4322c73a455
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-03-05 21:54:22 +0000

    all FAQ information from chloe input

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,7 +1,14 @@
 export default function Faq() {
   return (
     <div>
-        
+        <h3>When is the RSVP deadline?</h3>
+        <p>Please RSVP by 1 st May 2025, so we can have an accurate headcount.</p>
+        <h3>Can i bring a plus one?</h3>
+        <p>Please check your invite, all plus ones are named.</p>
+        <h3>Are Kids Welcome?</h3>
+        <p>No, we are only inviting the children of our immediate family for our wedding day.</p>
+        <h3>Who do we contact with and further questions?</h3>
+        <p>Dave- 07577452638</p>
     </div>
   )
 }
