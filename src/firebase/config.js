e976b646647b37ad3e9d8b9b78cdfed47e3121ce
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-03-06 14:51:58 +0000

    code layout changed

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,19 +2,11 @@ import firebase from 'firebase/app';
 import 'firebase/firestore'
 
 const firebaseConfig = {
-
     apiKey: "AIzaSyAHDTzxl1lW-Dy6quu7hLPpNy4G9gs-4Pc",
-  
     authDomain: "hester-wedding-site.firebaseapp.com",
-  
     projectId: "hester-wedding-site",
-  
     storageBucket: "hester-wedding-site.firebasestorage.app",
-  
     messagingSenderId: "177717928785",
-  
     appId: "1:177717928785:web:ef78e09b8b0f2dc003a970",
-  
     measurementId: "G-VTTBY9N5RM"
-  
   };
