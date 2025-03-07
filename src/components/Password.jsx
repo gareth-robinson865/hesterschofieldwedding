559966df035c1dd0ef0d926b5f6dcf9c35a263b8
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-03-07 13:19:44 +0000

    html created for the form

--- src/components/Password.jsx
+++ src/components/Password.jsx
@@ -2,7 +2,11 @@
 export default function Password({ setShowPassword }) {
   return (
     <div>
-        
+        <h3>We would love for you to join us on our Wedding day</h3>
+        <form>
+            <label htmlFor="password">Please enter the code on your invitation</label>
+            <input type="password"></input>
+        </form>
     </div>
   )
 }
