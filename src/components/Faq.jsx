85c57fdaa9a91f8b10624f3333f9fc3d3ba05df7
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-03-09 11:48:17 +0000

    REMOVED rsvp

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,8 +1,6 @@
 export default function Faq() {
   return (
     <div>
-        <h3>When is the RSVP deadline?</h3>
-        <p>Please RSVP by 1 st May 2025, so we can have an accurate headcount.</p>
         <h3>Can i bring a plus one?</h3>
         <p>Please check your invite, all plus ones are named.</p>
         <h3>Are Kids Welcome?</h3>
